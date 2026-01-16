586ac09b8d1078eacd1aa0f01ebff52478c280a3
sheryyiee121 <mylove0012211@gmail.com>
2026-01-16 14:05:25 -0500

    sdsf

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
+import Home from "./pages/Home/home";
 import Signup from "./pages/Signup/Signup";
 import Trending from "./pages/TrendingPage/TremdingPage";
 import Profile from "./pages/Reasons/Reasons";
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Signup" element={<Signup />} />
-         <Route path="/trending" element={<Trending />} />
+        <Route path="/trending" element={<Trending />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
